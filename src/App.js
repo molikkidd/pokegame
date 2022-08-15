1a32fbf0d66522909b2957205cb0794831e1725e
Molikkidd <mzkidd5@gmail.com>
2022-08-15 15:46:45 -0700

    add pokedex of characters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import Pokecard from './Pokecard';
+import React, { Component } from 'react';
+import Pokegame from './Pokegame';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Pokecard id={4} name="Charmander" type="fire" exp={62}/>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Pokegame />
+			</div>
+		);
+	}
 }
 
 export default App;
